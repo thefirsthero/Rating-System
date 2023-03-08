a7d40d3ad315f5c60791dada479f2d85877c97d7
vukosimoyane <126679577+vukosimoyane@users.noreply.github.com>
2023-03-08 08:01:13 +0000

    code structure and readability updates

--- register_and_login/test_app.py
+++ register_and_login/test_app.py
@@ -1,13 +1,14 @@
+'''The main function in which to run our application'''
+import os
 from datetime import datetime
 from flask import Flask, request, render_template, redirect, session
 from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
 import pytz
-import os
 from dotenv import load_dotenv
 
 load_dotenv() # load environment variables form .env
- 
+
 # create Flask app
 app = Flask(__name__)
 app.secret_key = "REDACTED"
@@ -30,7 +31,11 @@ id INT AUTO_INCREMENT,
 # register method does not allow for two people with the same username
 # therefore this method returns a tuple containing information from one row of a table
 def get_credentials_tuple(uname):
-    myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv('USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
+    '''This function returns the credentials of the user'''
+    myconn = mysql.connector.connect(host=os.getenv('HOST'),
+                                    user=os.getenv('USER'),
+                                    passwd=os.getenv('PASSWD'),
+                                    database=os.getenv('DATABASE'))
 
     cur = myconn.cursor()
     try:
@@ -40,7 +45,7 @@ def get_credentials_tuple(uname):
         myresult = cur.fetchone()
         # dic = { t[0] : t[1] for t in myresult}
 
-    except:
+    except mysql.connector.DatabaseError:
         myconn.rollback()
 
     myconn.close()
@@ -49,98 +54,102 @@ def get_credentials_tuple(uname):
 
 # dic = get_credentials_dict()
 
-# index page
 @app.route('/', methods=["GET", "POST"])
 def index():
+    '''This function returns the index page'''
     return render_template('index.html')
- 
-#login page
+
 @app.route('/login', methods=["GET", "POST"])
 def login():
- 
+    '''This function is responsible for validating a users loging credentials'''
     # retrieve info from login form
     if request.method == 'POST':
-        loginUN = request.form["lUN"]
-        loginPass = request.form["lPass"]
+        login_un = request.form["lUN"]
+        login_pass = request.form["lPass"]
 
-        my_tuple = get_credentials_tuple(loginUN)
+        my_tuple = get_credentials_tuple(login_un)
         # check if the username entered is registered in the database
-        if my_tuple != None:
+        if my_tuple is not None:
 
             # check if the password entered matches up to the hashed password in the database
-            if check_password_hash(my_tuple[1], loginPass):
-                print(f"Login of {loginUN} is valid!")
- 
+            if check_password_hash(my_tuple[1], login_pass):
+                print(f"Login of {login_un} is valid!")
+
                 # store login information on a cookie
-                session['username'] = loginUN
+                session['username'] = login_un
                 return redirect('/')
             else:
- 
+
                 # failed login
-                print(f'Login of {loginUN} is invalid!')
+                print(f'Login of {login_un} is invalid!')
                 return redirect('/')
         else:
- 
+
             # failed login
             print('User has not registered yet!')
             return redirect('/')
-        
+
 
     return render_template('login.html')
- 
+
 # code to log user out
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
+    '''This function is responsible for logging a user out'''
     try:
         # check if user is logged in
         if session['username']:
-    
+
             #log user out
             session.pop('username', default=None)
             session.pop('email', default=None)
         return redirect('/')
-    except:
+    except mysql.connector.DatabaseError:
         return redirect('/')
- 
+
 # recieve information from html form
 @app.route('/register', methods=["GET", "POST"])
 def register():
+    '''This function registers a user is our database'''
     session['register'] = 'registered'
     # global dic
-    
+
     if request.method == 'POST':
-        textPassword = request.form["txtPswd"]
-        textUsername = request.form["txtUN"]
+        text_password = request.form["txtPswd"]
+        text_username = request.form["txtUN"]
 
-        my_tuple = get_credentials_tuple(textUsername)
+        my_tuple = get_credentials_tuple(text_username)
         # check if the username exists in the db yet
-        if my_tuple == None:
+        if my_tuple is None:
 
-                # hash password
-                passwordHash = generate_password_hash(textPassword)
+            # hash password
+            password_hash = generate_password_hash(text_password)
 
-                # commit info to mysql
-                # dic[textUsername] = passwordHash
+            # commit info to mysql
+            # dic[text_username] = password_hash
 
-                myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv('USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
+            myconn = mysql.connector.connect(host=os.getenv('HOST'),
+                                            user=os.getenv('USER'),
+                                            passwd=os.getenv('PASSWD'),
+                                            database=os.getenv('DATABASE'))
 
-                cur = myconn.cursor()
+            cur = myconn.cursor()
 
-                query = 'INSERT INTO coach (coach_name, coach_password, register_date) VALUES (%s, %s, %s)'
-                val = [(textUsername, passwordHash, datetime.now(pytz.utc))]
-                cur.executemany(query, val)
+            query = 'INSERT INTO coach (coach_name, coach_password, register_date) VALUES (%s, %s, %s)'
+            val = [(text_username, password_hash, datetime.now(pytz.utc))]
+            cur.executemany(query, val)
 
-                myconn.commit()
-                
-                # return to index form
-                #dic = get_credentials_dict()
-                return redirect('/')
+            myconn.commit()
+
+            # return to index form
+            #dic = get_credentials_dict()
+            return redirect('/')
         else:
             print('Username is taken!')
             session.pop('register', default=None)
 
     return render_template('register.html')
- 
+
 # generic Flask app guard
 if __name__ == '__main__':
     app.run(debug=True)
