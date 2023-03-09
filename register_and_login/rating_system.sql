2c8ffedffc230db7db11c487fcfa8351740fa919
HactorPK <101172691+HactorPK@users.noreply.github.com>
2023-03-09 11:35:13 +0200

    Update rating_system.sql

--- register_and_login/rating_system.sql
+++ register_and_login/rating_system.sql
@@ -5,6 +5,7 @@ create table coach (
 coach_id int not null auto_increment,
 coach_name varchar(100) not null,
 coach_password varchar(500) not null,
+user_type varchar(10),
 coach_avg_rating float,
 register_date DATETIME,
 primary key (coach_id)
@@ -14,6 +15,7 @@ create table coachee (
 coachee_id int not null auto_increment,
 coachee_name varchar(100) not null,
 coachee_password varchar(500) not null,
+user_type varchar(10),
 coachee_avg_rating float,
 register_date DATETIME,
 primary key (coachee_id)
