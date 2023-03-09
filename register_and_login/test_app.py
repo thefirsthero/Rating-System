71ff260133c4a6149180ca9ec9e3b294d195eacd
HactorPK <101172691+HactorPK@users.noreply.github.com>
2023-03-09 11:33:37 +0200

    Update test_app.py

--- register_and_login/test_app.py
+++ register_and_login/test_app.py
@@ -1,13 +1,13 @@
-'''The main function in which to run our application'''
-import os
 from datetime import datetime
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
 import pytz
+import os
 from dotenv import load_dotenv
-
-load_dotenv() # load environment variables form .env
+from tkinter import *
+import tkinter.messagebox
+load_dotenv()  # load environment variables form .env
 
 # create Flask app
 app = Flask(__name__)
@@ -16,33 +16,25 @@ app.secret_key = "REDACTED"
 # user credentials
 # credentials = {'john':'123', 'amy': '123', 'admin':'admin'}
 
-
-'''
-CREATE DATABASE university;
-CREATE TABLE users(
-id INT AUTO_INCREMENT,
-   username VARCHAR(100),
-   password VARCHAR(500),
-   register_date DATETIME,
-   PRIMARY KEY(id);
-'''
-
 # read first name and password from table into tuple
 # register method does not allow for two people with the same username
 # therefore this method returns a tuple containing information from one row of a table
+
+
 def get_credentials_tuple(uname):
-    '''This function returns the credentials of the user'''
-    myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv('USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
+    myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv(
+        'USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
 
     cur = myconn.cursor()
     try:
-        query = "select coach_name, coach_password from coach where coach_name = '" + uname +"';"
+        query = "select coach_name, coach_password from coach where coach_email = '" + uname + "';"
         cur.execute(query)
 
         myresult = cur.fetchone()
         # dic = { t[0] : t[1] for t in myresult}
 
-    except mysql.connector.DatabaseError:
+    except:
+        myresult = None
         myconn.rollback()
 
     myconn.close()
@@ -51,99 +43,120 @@ def get_credentials_tuple(uname):
 
 # dic = get_credentials_dict()
 
+# index page
+
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    '''This function returns the index page'''
     return render_template('index.html')
+
+# login page
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    '''This function is responsible for validating a users loging credentials'''
+    root = Tk()
+
     # retrieve info from login form
     if request.method == 'POST':
-        login_un = request.form["lUN"]
-        login_pass = request.form["lPass"]
+        login_email = request.form.get("email")
+        login_password = request.form.get('user_password')
 
-        my_tuple = get_credentials_tuple(login_un)
-        # check if the username entered is registered in the database
-        if my_tuple is not None:
-
+        my_tuple = get_credentials_tuple(login_email)
+        # check if the email entered is registered in the database
+        if my_tuple != None:
             # check if the password entered matches up to the hashed password in the database
-            if check_password_hash(my_tuple[1], login_pass):
-                print(f"Login of {login_un} is valid!")
+            if check_password_hash(my_tuple[1], login_password):
+                print(f"Login of {login_email} is valid!")
 
                 # store login information on a cookie
-                session['username'] = login_un
-                return render_template('home.html')
+                session['email'] = login_email
+                return redirect(url_for('success'))
             else:
 
                 # failed login
-                print(f'Login of {login_un} is invalid!')
-                return redirect('/')
+                
+                print(f'Login of {login_email} is invalid!')
+                return render_template('login.html', error_msg = "Incorrect password, try again.")
         else:
-
+            return render_template("login.html", error_msg = "User has not registered yet")
             # failed login
-            print('User has not registered yet!')
-            return redirect('/')
-
+            # print('User has not registered yet!')
 
     return render_template('login.html')
 
 # code to log user out
+
+
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
-    '''This function is responsible for logging a user out'''
     try:
         # check if user is logged in
         if session['username']:
 
-            #log user out
+            # log user out
             session.pop('username', default=None)
             session.pop('email', default=None)
         return redirect('/')
-    except mysql.connector.DatabaseError:
+    except:
         return redirect('/')
 
 # recieve information from html form
+
+
 @app.route('/register', methods=["GET", "POST"])
 def register():
-    '''This function registers a user is our database'''
+    #cookie
     session['register'] = 'registered'
     # global dic
 
     if request.method == 'POST':
-        text_password = request.form["txtPswd"]
-        text_username = request.form["txtUN"]
+        textName = request.form.get('first-name')
+        textLName = request.form.get('last-name')
+        textEmail = request.form.get('email')
+        textPassword = request.form.get('new-password')
+        userType = ''
+        if (request.form.get('coach') != ""):
+            userType = 'coachee'
+        else:
+            userType = 'coach'
 
-        my_tuple = get_credentials_tuple(text_username)
+        my_tuple = get_credentials_tuple(textEmail)
+
         # check if the username exists in the db yet
-        if my_tuple is None:
+        if my_tuple == None:
 
             # hash password
-            password_hash = generate_password_hash(text_password)
+            passwordHash = generate_password_hash(textPassword)
 
             # commit info to mysql
-            # dic[text_username] = password_hash
-
-            myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv('USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
+            # dic[textUsername] = passwordHash
+#sess
+            myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv(
+                'USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
 
             cur = myconn.cursor()
 
-            query = 'INSERT INTO coach (coach_name, coach_password, register_date) VALUES (%s, %s, %s)'
-            val = [(text_username, password_hash, datetime.now(pytz.utc))]
+            query = 'INSERT INTO coach (coach_name, coach_lastname, coach_email, coach_password, user_type, register_date) VALUES (%s, %s, %s, %s, %s ,%s)'
+            val = [(textName, textLName, textEmail, passwordHash,
+                    userType,  datetime.now(pytz.utc))]
             cur.executemany(query, val)
 
             myconn.commit()
 
             # return to index form
-            #dic = get_credentials_dict()
+            # dic = get_credentials_dict()
             return redirect('/')
         else:
-            print('Username is taken!')
-            session.pop('register', default=None)
+            print('Email already registered, please log in!')
+            return redirect(url_for('login'))
 
     return render_template('register.html')
-
 
 
 # generic Flask app guard
