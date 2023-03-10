4da67d510552cc0fb6a6e88fb92076ccf80140dd
vukosimoyane <vukosi.moyane@sanlam.co.za>
2023-03-10 14:21:27 +0200

    fixing stuff

--- app/app.py
+++ app/app.py
@@ -21,7 +21,7 @@ logging.CRITICAL
 
 logger = logging.getLogger(__name__)
 FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
-logging.basicConfig(format=FORMAT)
+logging.basicConfig(filename='log/record.log', filemode='w',format=FORMAT)
 logger.setLevel(logging.WARNING)
 
 # create Flask app
@@ -36,17 +36,48 @@ app.secret_key = "REDACTED"
 # therefore this method returns a tuple containing information from one row of a table
 
 
-def get_credentials_tuple(uname):
+def get_credentials_tuple(email):
+    UserType = None
     myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv(
         'USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
 
     cur = myconn.cursor()
     try:
-        query = "select coach_name, coach_password from coach where coach_email = '" + uname + "';"
+        
+        query = 'SELECT coach_name, coach_password FROM coach WHERE coach_email = "' + email + '";'
         cur.execute(query)
+        myresult1 = cur.fetchone()
 
+        query = 'SELECT coachee_name, coachee_password FROM coachee WHERE coachee_email = "' + email + '";'
+        cur.execute(query)
+        myresult2 = cur.fetchone()
+
+    except:
+        myresult1 = None
+        myresult2 = None
+        myconn.rollback()
+
+    myconn.close()
+
+    if ((myresult1 == None) and (myresult2 == None)):
+        print('1')
+        return myresult1
+    elif ((myresult1 != None) and (myresult2 == None)):
+        print('1')
+        return myresult1
+    elif ((myresult1 == None) and (myresult2 != None)):
+        print('1')
+        return myresult2
+
+def ifEmailExists(email):
+    myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv(
+        'USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
+
+    cur = myconn.cursor()
+    try:
+        query = 'SELECT coach_email FROM coach WHERE coach.coach_email = "' + email + '" UNION SELECT coachee_email FROM coachee WHERE coachee_email = "' + email + '";'
+        cur.execute(query)
         myresult = cur.fetchone()
-        # dic = { t[0] : t[1] for t in myresult}
 
     except:
         myresult = None
@@ -54,9 +85,11 @@ def get_credentials_tuple(uname):
 
     myconn.close()
 
-    return myresult
-
-# dic = get_credentials_dict()
+    if myresult is None:
+        return False
+    else:
+        return True
+    
 
 # index page
 
@@ -86,18 +119,21 @@ def login():
         if my_tuple != None:
             # check if the password entered matches up to the hashed password in the database
             if check_password_hash(my_tuple[1], login_password):
-                print(f"Login of {login_email} is valid!")
+                # print(f"Login of {login_email} is valid!")
 
                 # store login information on a cookie
                 session['email'] = login_email
+                logger.warning('Logged in Succesfully!')
                 return redirect(url_for('success'))
             else:
 
                 # failed login
                 
-                print(f'Login of {login_email} is invalid!')
+                # print(f'Login of {login_email} is invalid!')
+                logger.warning('Incorrect password, try again!')
                 return render_template('login.html', error_msg = "Incorrect password, try again.")
         else:
+            logger.warning('User has not registered yet!')
             return render_template("login.html", error_msg = "User has not registered yet")
             # failed login
             # print('User has not registered yet!')
@@ -135,17 +171,16 @@ def register():
         textPassword = request.form.get('new-password')
         userType = ''
 
-        chosenValue = request.form.get('user-type')
-        
-        if chosenValue == "optionCoach":
-            userType = "coach"
+        if 'coach1' in request.form:
+            userType = 'coach'
         else:
             userType = 'coachee'
 
-        my_tuple = get_credentials_tuple(textEmail)
+        emailExists = ifEmailExists(textEmail)
+        # print(emailExists)
 
         # check if the username exists in the db yet
-        if my_tuple == None:
+        if emailExists == False:
 
             # hash password
             passwordHash = generate_password_hash(textPassword)
@@ -158,7 +193,7 @@ def register():
 
             cur = myconn.cursor()
 
-            query = 'INSERT INTO coach (coach_name, coach_surname, coach_email, coach_password, user_type, register_date) VALUES (%s, %s, %s, %s, %s ,%s)'
+            query = 'INSERT INTO ' + userType + ' (' + userType + '_name,' + userType + '_surname,' + userType + '_email,' + userType + '_password, user_type, register_date) VALUES (%s, %s, %s, %s, %s ,%s)'
             val = [(textName, textLName, textEmail, passwordHash,
                     userType,  datetime.now(pytz.utc))]
             cur.executemany(query, val)
@@ -167,9 +202,10 @@ def register():
 
             # return to index form
             # dic = get_credentials_dict()
+            logger.warning('Successfully registered!')
             return redirect('/')
         else:
-            print('Email already registered, please log in!')
+            logger.warning('Email already registered, please log in!')
             return redirect(url_for('login'))
 
     return render_template('register.html')
