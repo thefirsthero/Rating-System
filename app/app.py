1871d6d8c3a9e6285bedc025e4e0b95dafdfc4fb
vukosimoyane <vukosi.moyane@sanlam.co.za>
2023-03-14 14:03:39 +0200

    bare bones viewing of ratings given & received

--- app/app.py
+++ app/app.py
@@ -231,19 +231,14 @@ def go_to_create_rating():
 
 @app.route('/go_to_view_ratings_recieved')
 def go_to_view_ratings_recieved():
-    # get opposite user type
-    if session['user_type'] == 'coach':
-        opposite_user_type = 'coachee'
-    else:
-        opposite_user_type = 'coach'
-    
+    '''Collecting the comments from the database and passing it to the html form'''
     # Initialise mysql
     myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv(
                 'USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
 
     cur = myconn.cursor()
     
-    # Get their id form db
+    # Get their id from db
     their_id = None
     coach_rated = False
     if session['user_type'] == 'coach':
@@ -254,25 +249,64 @@ def go_to_view_ratings_recieved():
         # updating coach_rated boolean
         coach_rated = True
     else:
-        # get ratee id
+        # get their id
         query = 'SELECT coachee_id FROM coachee WHERE coachee_email = "' + session['email'] + '";'
         cur.execute(query)
         their_id = cur.fetchone()[0]
     
-    # create a connection
-    myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv(
-                'USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
-    #create a cursor
-    cursor = myconn.cursor() 
     #execute select statement to fetch data to be displayed in combo/dropdown
-    cursor.execute('SELECT rating_comment, star_rating FROM rating WHERE (' + session['user_type'] +'_id_fk = "' + str(their_id) + '") AND (coach_rated = '+str(coach_rated)+')') 
+    cur.execute('SELECT rating_comment, star_rating FROM rating WHERE (' + session['user_type'] +'_id_fk = "' + str(their_id) + '") AND (coach_rated = '+str(coach_rated)+')') 
     #fetch all rows ans store as a set of tuples 
-    commentlist = cursor.fetchall() 
-    return render_template('view_rating_received.html', commentlist=commentlist)
+    commentlist = cur.fetchall() 
+
+    '''Collecting the avg rating from the database and passing it to the html form'''
+    # Get their avg from db
+    avg_rating = None
+
+    if session['user_type'] == 'coach':
+        # if they are a coach
+        query = 'SELECT coach_avg_rating FROM coach WHERE coach_email = "' + session['email'] + '";'
+        cur.execute(query)
+        avg_rating = cur.fetchone()[0]
+    else:
+        # get ratee id
+        query = 'SELECT coachee_avg_rating FROM coachee WHERE coachee_email = "' + session['email'] + '";'
+        cur.execute(query)
+        avg_rating = cur.fetchone()[0]
+
+    return render_template('view_rating_received.html', commentlist=commentlist, avg_rating=avg_rating)
 
 @app.route('/go_to_view_ratings_given')
 def go_to_view_ratings_given():
-    return render_template('home.html')
+    '''Collecting the comments from the database and passing it to the html form'''
+    # Initialise mysql
+    myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv(
+                'USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
+
+    cur = myconn.cursor()
+    
+    # Get their id from db
+    their_id = None
+    coach_rated = False
+    if session['user_type'] == 'coach':
+        # if they are a coach
+        query = 'SELECT coach_id FROM coach WHERE coach_email = "' + session['email'] + '";'
+        cur.execute(query)
+        their_id = cur.fetchone()[0]
+        # updating coach_rated boolean
+        coach_rated = True
+    else:
+        # get their id
+        query = 'SELECT coachee_id FROM coachee WHERE coachee_email = "' + session['email'] + '";'
+        cur.execute(query)
+        their_id = cur.fetchone()[0]
+    
+    #execute select statement to fetch data to be displayed in combo/dropdown
+    cur.execute('SELECT rating_comment, star_rating FROM rating WHERE (' + session['user_type'] +'_id_fk = "' + str(their_id) + '") AND (coach_rated = '+str(not coach_rated)+')') 
+    #fetch all rows ans store as a set of tuples 
+    commentlist = cur.fetchall() 
+
+    return render_template('view_rating_given.html', commentlist=commentlist)
 
 # logic to create a rating
 @app.route('/create_rating',methods=['GET','POST'])
@@ -372,85 +406,9 @@ def create_rating():
 # logic to view received ratings
 @app.route('/view_rating_received',methods=['GET','POST'])
 def view_rating_received():
-    email = session['email']
-
     if request.method == 'GET':
+        pass
 
-        # Initialise mysql
-        myconn = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv(
-                    'USER'), passwd=os.getenv('PASSWD'), database=os.getenv('DATABASE'))
-
-        cur = myconn.cursor()
-
-        # declare coach rated boolean as True
-        coach_rated = True
-
-        # getting coachID or coacheeID for rater
-        raterID = None
-        if session['user_type'] == 'coach':
-            # if rater is a coach
-            query = 'SELECT coach_id FROM coach WHERE coach_email = "' + session['email'] + '";'
-            cur.execute(query)
-            raterID = cur.fetchone()[0]
-
-            # get ratee id
-            query = 'SELECT coachee_id FROM coachee WHERE coachee_email = "' + email + '";'
-            cur.execute(query)
-            rateeID = cur.fetchone()[0]
-
-            # set coach_rated boolean to False
-            coach_rated = False
-
-        else:
-            # if rater is a coachee
-            query = 'SELECT coachee_id FROM coachee WHERE coachee_email = "' + session['email'] + '";'
-            cur.execute(query)
-            raterID = cur.fetchone()[0]
-
-            # get ratee id
-            query = 'SELECT coach_id FROM coach WHERE coach_email = "' + email + '";'
-            cur.execute(query)
-            rateeID = cur.fetchone()[0]
-
-        # Populate table
-        # A coach has been rated
-        if coach_rated:
-            try:
-                    
-                query = 'INSERT INTO rating (coach_id_fk, coachee_id_fk, star_rating, rating_comment, coach_rated, coachee_rated, register_date) VALUES (%s, %s, %s, %s, %s, %s, %s)'
-                val = [(rateeID, raterID, rating, comment, coach_rated, (not coach_rated),datetime.now(pytz.utc))]
-                cur.executemany(query, val)
-
-                myconn.commit()
-
-                
-            except:
-                myconn.rollback()
-
-            myconn.close()
-            # A coachee has been rated
-        else:
-            try:
-                    
-                query = 'INSERT INTO rating (coach_id_fk, coachee_id_fk, star_rating, rating_comment, coach_rated, coachee_rated, register_date) VALUES (%s, %s, %s, %s, %s, %s, %s)'
-                val = [(raterID, rateeID, rating, comment, coach_rated, (not coach_rated),datetime.now(pytz.utc))]
-                cur.executemany(query, val)
-
-                myconn.commit()
-
-                
-            except:
-                myconn.rollback()
-
-            myconn.close()
-
-
-        return redirect(url_for('success'))
-    else:
-        # Failed rating
-        flash('Failed to create rating, pleae try again!')
-        logger.warning('Failed to create rating, pleae try again!')
-        return render_template('rating.html')
 # generic Flask app guard
 if __name__ == '__main__':
     app.run(debug=True)
